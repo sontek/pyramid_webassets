3349a11eade95339d7774b8734350fc2f8b67709
Michael Merickel <michael@merickel.org>
2013-03-31 23:15:34 -0500

    cleanup setup.py for fun

--- setup.py
+++ setup.py
@@ -1,20 +1,14 @@
 import os
-import sys
 
 from setuptools import setup, find_packages, Command
 
 here = os.path.abspath(os.path.dirname(__file__))
 
-def _read(path):
-    with open(path) as f:
-        data= f.read()
+with open(os.path.join(here, 'README.md')) as fp:
+    README = fp.read()
 
-    f.close()
-
-    return data
-
-README = _read(os.path.join(here, 'README.md'))
-CHANGES = _read(os.path.join(here, 'CHANGES.txt'))
+with open(os.path.join(here, 'CHANGES.txt')) as fp:
+    CHANGES = fp.read()
 
 #requires = open('requirements.txt').readlines()
 requires = ['pyramid>=1.3', 'webassets>=0.7.1', 'zope.interface']
@@ -33,13 +27,13 @@ class PyTest(Command):
 setup(name='pyramid_webassets',
       version='0.7',
       description='pyramid_webassets',
-      long_description=README + '\n\n' +  CHANGES,
+      long_description=README + '\n\n' + CHANGES,
       classifiers=[
-        "Programming Language :: Python",
-        "Framework :: Pylons",
-        "Topic :: Internet :: WWW/HTTP",
-        "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
-        ],
+          "Programming Language :: Python",
+          "Framework :: Pylons",
+          "Topic :: Internet :: WWW/HTTP",
+          "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
+      ],
       author='John Anderson',
       author_email='sontek@gmail.com',
       url='http://github.com/sontek/pyramid_webassets',
@@ -48,9 +42,7 @@ setup(name='pyramid_webassets',
       include_package_data=True,
       zip_safe=False,
       test_suite='pyramid_webassets',
-      install_requires = requires,
-      test_requires = ['pytest', 'mock', 'unittest2'],
-      cmdclass = {'test': PyTest},
-      paster_plugins=['pyramid'],
+      install_requires=requires,
+      tests_require=['pytest', 'mock', 'unittest2'],
+      cmdclass={'test': PyTest},
       )
-
